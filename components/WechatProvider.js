f95c311c46acb2b95a662e5fcd5fbf116576d9d3
josh <postor@gmail.com>
2018-01-10 18:19:18 +0800

    basic

---
+++ components/WechatProvider.js
@@ -0,0 +1,95 @@
+import { Component, Children } from 'react'
+import Head from 'next/head'
+import PropTypes from 'prop-types'
+import { isFunction } from 'lodash'
+import WechatJSSDK from './client'
+
+class WechatProvider extends Component {
+
+  static propTypes = {
+    config: PropTypes.any.isRequired,
+    debug: PropTypes.bool,
+    success: PropTypes.func,
+    error: PropTypes.func,
+  }
+
+  static childContextTypes = {
+    isWxready: PropTypes.bool,
+    isWechat: PropTypes.bool,
+    onWxReady: PropTypes.func,
+    wx: PropTypes.any,
+  }
+
+  constructor(props) {
+    super(props)
+
+    const isWechat = typeof navigator != 'undefined' && /micromessenger/.test(navigator.userAgent.toLowerCase())
+    this.state = {
+      ready: false,
+      isWechat,
+    }
+
+    this.readyListenners = []
+    this.wx = null
+  }
+
+  getChildContext() {
+    const { ready, isWechat } = this.state
+    const onWxReady = (callback) => this.addOnReady(callback)
+    return {
+      isWxready: ready,
+      isWechat,
+      onWxReady,
+      wx: this.wx,
+    }
+  }
+
+  render() {
+    return Children.only(this.props.children)
+  }
+
+  componentWillMount() {
+    const { isWechat } = this.state
+    const { config } = this.props
+    if (!isWechat || !config) {
+      return
+    }
+
+    if (isFunction(config)) {
+      config((c) => this.initWx(c))
+    } else {
+      this.initWx(config)
+    }
+  }
+
+  addOnReady(callback) {
+    const { ready } = this.state
+
+    this.readyListenners.push(callback)
+
+    if (ready) {
+      callback(this.wx)
+    }
+  }
+
+  initWx(config) {
+    const { success = () => { }, error = () => { }, debug = false } = this.props
+    const s = () => {
+      this.setState({
+        ready: true,
+      })
+      this.readyListenners.forEach((x) => x(this.wx))
+      success(this.wx)
+    }
+
+    this.wx = new WechatJSSDK({
+      ...config,
+      success: s,
+      error,
+      debug,
+    })
+  }
+
+}
+
+export default WechatProvider
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,35 @@
-export default ()=>(<div>
-  hahaha
+import request from 'superagent'
+
+import WP from '../components/WechatProvider'
+import config from '../config'
+
+
+const { appId } = config
+
+const getConfig = (cb) => {
+  if(typeof window == 'undefined'){
+    return
+  }
+  
+  const api = `/get-ticket`
+  request(api)
+  .query({url:location.href})
+  .then((data) => {
+    return {
+      ...data,
+      appId,
+      debug: true,
+    }
+  }).then((c) => {
+    cb(c)
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+
+export default () => (<div>
+  <WP config={getConfig} success={(wx) => alert('success')} error={(err) => alert('error')} debug={true}>
+    <h1>test WechatProvider</h1>
+
+  </WP>
 </div>)
